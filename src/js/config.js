1e6f4605ed3838fa2cbb2d050cc4b76be32379b8
EliezerAmbion <eliezerambion@gmail.com>
2021-04-28 13:34:20 +0800

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '1d868a7b-bcb2-4d53-8578-021d84ab98cf';
+export const MODAL_CLOSE_SEC = 2.5;
